659f2c2b6e7b0f5560f08b2478e5bdde5cc0b61f
harshad241200 <harshadpatil242412@gmail.com>
2022-10-11 11:21:22 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+include_once 'College.php';
+
+$crud = new College;
+$result = $crud->displayAllColleges();
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">College Name</th>
+      <th scope="col">College Type</th>
+      <th scope="col">College Facilities</th>
+      <th scope="col">Image</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+<?php 
+if($result !== false){
+
+  foreach($result as $data){
+
+?>
+
+<tr>
+<td><?=$data['id']?></td>
+  <td><?=$data['collegename']?></td>
+  <td><?=$data['collegetype']?></td>
+  <td><?=$data['collegefacilities']?></td>
+  <td><image width="100" height="100" src="uploads/<?=$data['collegeimage']?>"></td>
+  <td><a href="edit.php?id=<?=$data['id']?>">edit</a></td>
+  <td><a href="delete.php?id=<?=$data['id']?>">delete</a></td>
+
+</tr>
+
+<?php
+  }
+
+  }
+  else{
+  ?>
+
+  <tr><td colspan="4">No data found!!</td></tr>
+
+  <?php
+  }
+  ?>
+
+
+  </tbody>
+</table>
+
+
+<button type="submit">Submit</button>
+
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+  </body>
+</html>
